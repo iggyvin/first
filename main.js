b7fdd60aa05d364428fcbf43a7fa850bc364dd4f
iggyvin <iggyvinted@gmail.com>
2017-04-18 15:47:33 +0200

    check

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* -------- MAP --------- */
+// -------- MAP ---------
 
 function initMap() {
 	var spot = {lat: 40.71, lng: -74};
@@ -16,9 +16,10 @@ window.initMap=initMap;
 
 
 
-/* -------- CAROUSEL -------- */ 
+// -------- CAROUSEL -------- 
 var myIndex = 0;
 carousel();
+//carousel("slides", "badge", "none", "transparent", "block", "rgb(241, 101, 34)");
 
 function carousel() {
     var i;
@@ -31,44 +32,51 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";
-	dots[myIndex-1].style.backgroundColor = "rgb(241, 101, 34)"
+	dots[myIndex-1].style.backgroundColor = "rgb(241, 101, 34)";
     setTimeout(carousel, 4500);
 }
 
-
-
-/* -------- CLICK -------- */
-var slideIndex = 1;
-showDivs(slideIndex);
+// -------- CLICK -------- 
+var slideIndex1 = 1;
+var slideIndex2 = 1;
+showDivs1(slideIndex1);
+showDivs2(slideIndex2);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+	showDivs1(slideIndex1 += n);
+	showDivs2(slideIndex2 += n);
+}	
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+	showDivs1(slideIndex1 = n);
+	showDivs2(slideIndex2 = n);
 }
 
-function showDivs(n) {
+function showDivs1(n) {
 	var i;
   	var x = document.getElementsByClassName("slides");
   	var dots = document.getElementsByClassName("badge");
-  	if (n > x.length) {slideIndex = 1}
-  	if (n < 1) {slideIndex = x.length}
+  	if (n > x.length) {slideIndex1 = 1}
+  	if (n < 1) {slideIndex1 = x.length}
   	for (i = 0; i < x.length; i++) {
-     	x[i].style.display = "none";
+     	x[i].style.display =  "none";
 		dots[i].style.backgroundColor = "transparent";
   	}
-	
-  	x[slideIndex-1].style.display = "block";
-  	dots[slideIndex-1].style.backgroundColor = "rgb(241, 101, 34)";
-	
+  	x[slideIndex1-1].style.display = "block";
+  	dots[slideIndex1-1].style.backgroundColor = "rgb(241, 101, 34)";
 }
 
-
-
-
-/* ---------- CLIENTS ----------- */
+function showDivs2(n) {
+	var i;
+  	var x = document.getElementsByClassName("sl");
+  	if (n > x.length) {slideIndex2 = 1}
+  	if (n < 1) {slideIndex2 = x.length}
+  	for (i = 0; i < x.length; i++) {
+     	x[i].style.display =  "none";
+  	}
+  	x[slideIndex2-1].style.display = "flex";
+}
+// ---------- CLIENTS ----------- 
 
 function showText(args) {
 	var i;
@@ -86,3 +94,27 @@ function hideText(args) {
 		x[i].style.display="none";
 	}
 }
+
+
+
+
+// ------ BLOG LOAD MORE ---- 
+
+
+$(document).ready(function () {
+    size_li = $("#notes .blog-note").size();
+    x=3;
+    $('#notes .blog-note:lt('+x+')').show();
+    $('.load-more').click(function () {
+        x= (x+3 <= size_li) ? x+3 : size_li;
+        $('#notes .blog-note:lt('+x+')').show();
+    });
+    $('.load-less').click(function () {
+        x=(x-3<0) ? 3 : x-3;
+        $('#notes .blog-note').not(':lt('+x+')').hide();
+    });
+});
+  
+  
+  
+
